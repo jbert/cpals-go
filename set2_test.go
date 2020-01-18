9ef45e01ac4b345995ad3bdc8c97d59cb4f2249f
John Berthels <john.berthels@gmail.com>
2020-01-18 17:55:12 +0000

    Add example from S2C9

--- set2_test.go
+++ set2_test.go
@@ -6,41 +6,53 @@ import (
 )
 
 func TestS2C9(t *testing.T) {
-	blockSize := 8
+	yellowSub := []byte("YELLOW SUBMARINE")
 
 	testCases := []struct {
-		in       []byte
-		expected []byte
+		in        []byte
+		expected  []byte
+		blockSize int
 	}{
 		{
 			[]byte{0, 0, 0, 0, 0, 0, 0, 0},
 			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8, 8, 8},
+			8,
 		},
 		{
 			[]byte{0, 0, 0, 0, 0, 0, 0},
 			[]byte{0, 0, 0, 0, 0, 0, 0, 1},
+			8,
 		},
 		{
 			[]byte{0, 0, 0, 0, 0, 0},
 			[]byte{0, 0, 0, 0, 0, 0, 2, 2},
+			8,
 		},
 		{
 			[]byte{0, 0, 0},
 			[]byte{0, 0, 0, 5, 5, 5, 5, 5},
+			8,
 		},
 		{
 			[]byte{},
 			[]byte{8, 8, 8, 8, 8, 8, 8, 8},
+			8,
 		},
 		{
 			[]byte{0},
 			[]byte{0, 7, 7, 7, 7, 7, 7, 7},
+			8,
+		},
+		{
+			yellowSub,
+			append(yellowSub, []byte{4, 4, 4, 4}...),
+			20,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("%v", tc.in), func(t *testing.T) {
-			got := BytesPKCS7Pad(tc.in, blockSize)
+			got := BytesPKCS7Pad(tc.in, tc.blockSize)
 			if !BytesEqual(got, tc.expected) {
 				t.Fatalf("got %v expected %v", got, tc.expected)
 			}
