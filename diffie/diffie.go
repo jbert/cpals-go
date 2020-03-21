6baf64490d56d557f1b715b88594b3c78141d471
John Berthels <john.berthels@gmail.com>
2020-03-21 12:06:04 +0000

    S5C35
    - I thought I could get the reply messages but I think I was wrong
    
    e.g. g=1 => we know what B will be, but not b
    so we don't know b^A, i.e. B's session key

--- diffie/diffie.go
+++ diffie/diffie.go
@@ -8,6 +8,9 @@ import (
 	"github.com/jbert/cpals-go/cbig"
 )
 
+var nistP = cbig.BigFromHex("25")
+
+/*
 var nistP = cbig.BigFromHex(`
 ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
 e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
@@ -18,6 +21,7 @@ c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
 bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
 fffffffffffff
 `)
+*/
 
 var nistG = big.NewInt(2)
 
@@ -32,6 +36,10 @@ type Reply struct {
 	B *big.Int
 }
 
+type Client interface {
+	Connect(Server)
+}
+
 type Server interface {
 	Handshake(Request) Reply
 	Call([]byte) []byte
@@ -39,6 +47,7 @@ type Server interface {
 
 type Cryptor struct {
 	g, p, a, A, B *big.Int
+	role          string
 }
 
 func (c Cryptor) String() string {
@@ -46,18 +55,26 @@ func (c Cryptor) String() string {
 		cbig.BigStr(c.a), cbig.BigStr(c.A), cbig.BigStr(c.B), cbig.BigStr(c.p), cbig.BigStr(c.g))
 }
 
+func bigIntToKey(n *big.Int) []byte {
+	hashN := cpals.SHA1(n.Bytes())
+	if len(hashN) < 16 {
+		panic(fmt.Sprintf("Unexpected hash key length: %d", len(hashN)))
+	}
+	key := hashN[0:16]
+	//log.Printf("bigIntToKey %s -> %v", n, key)
+	return key
+}
+
 func (c Cryptor) SessionKey(purpose string) []byte {
 	s := cbig.BigExpMod(c.B, c.a, c.p)
-	hashS := cpals.SHA1(s.Bytes())
-	if len(hashS) < 16 {
-		panic(fmt.Sprintf("Unexpected hash key length: %d", len(hashS)))
-	}
-	return hashS[0:16]
+	key := bigIntToKey(s)
+	//log.Printf("%s: B %s a %s p %s s %s: %v\n", purpose, c.B, c.a, c.p, s, key)
+	return key
 }
 
 func (c Cryptor) Encrypt(s string) []byte {
 	msg := []byte(s)
-	key := c.SessionKey("ENC")
+	key := c.SessionKey(fmt.Sprintf("%s ENC", c.role))
 	iv := cpals.RandomKey()
 	buf := cpals.AESCBCEncrypt(key, iv, msg)
 	return append(buf, iv...)
@@ -69,8 +86,14 @@ func splitIV(buf []byte, n int) ([]byte, []byte) {
 	return iv, buf
 }
 
-func (c Cryptor) Decrypt(buf []byte) string {
-	key := c.SessionKey("DEC")
+func (c Cryptor) Decrypt(buf []byte) (smsg string) {
+	defer func() {
+		if r := recover(); r != nil {
+			smsg = fmt.Sprintf("Can't decrypt: %s", r)
+		}
+	}()
+
+	key := c.SessionKey(fmt.Sprintf("%s DEC", c.role))
 	iv, buf := splitIV(buf, len(key))
 	msg := cpals.AESCBCDecrypt(key, iv, buf)
 	return string(msg)
@@ -90,10 +113,11 @@ func NewHonestClient() *HonestClient {
 
 	hc := &HonestClient{
 		Cryptor: Cryptor{
-			g: g,
-			p: p,
-			a: a,
-			A: A,
+			g:    g,
+			p:    p,
+			a:    a,
+			A:    A,
+			role: "HC",
 		},
 	}
 	return hc
@@ -120,29 +144,30 @@ type HonestServer struct {
 }
 
 func NewHonestServer() *HonestServer {
-	p := nistP
-	g := nistG
-
-	a := cbig.BigRand(p)
-	A := cbig.BigExpMod(g, a, p)
-
 	hs := &HonestServer{
-		Cryptor: Cryptor{
-			a: a,
-			A: A,
-		},
+		// Set up cryptor in Handshake
 		Replies: map[string]string{
 			"default": "Not initialised :-(",
+		},
+		Cryptor: Cryptor{
+			role: "HS",
 		},
 	}
 	return hs
 }
 
 func (hs *HonestServer) Handshake(hello Request) Reply {
+	// Use negotiated values. That will be safe.
 	hs.g = hello.g
 	hs.p = hello.p
 	// My remote pubkey is your local pubkey
 	hs.B = hello.A
+
+	hs.a = cbig.BigRand(hs.p)
+	hs.A = cbig.BigExpMod(hs.g, hs.a, hs.p)
+
+	//log.Printf("HS: %s", hs)
+
 	// I reply with my pubkey (A) as the remote pubkey (B)
 	return Reply{
 		B: hs.A,
@@ -162,11 +187,18 @@ func (hs *HonestServer) Call(buf []byte) []byte {
 	return replyBuf
 }
 
+type MITM interface {
+	Client
+	Server
+	SnoopedMessages() []string
+}
+
 type EvilMITM struct {
 	Cryptor
 	server Server
 
-	savedMessages [][]byte
+	aSavedMessages [][]byte
+	bSavedMessages [][]byte
 }
 
 func NewEvilMITM() *EvilMITM {
@@ -197,9 +229,9 @@ func (mitm *EvilMITM) Handshake(hello Request) Reply {
 }
 
 func (mitm *EvilMITM) Call(msg []byte) []byte {
-	mitm.savedMessages = append(mitm.savedMessages, msg)
+	mitm.aSavedMessages = append(mitm.aSavedMessages, msg)
 	reply := mitm.server.Call(msg)
-	mitm.savedMessages = append(mitm.savedMessages, reply)
+	mitm.bSavedMessages = append(mitm.bSavedMessages, reply)
 	return reply
 }
 
@@ -208,13 +240,164 @@ func (mitm *EvilMITM) SnoopedMessages() []string {
 	// Both sides are using pubkey == p
 	// key is (B ** a) %p == (p**a) %p == 0
 	s := big.NewInt(0)
-	hashS := cpals.SHA1(s.Bytes())
-	key := hashS[0:16]
+	key := bigIntToKey(s)
 
-	for _, buf := range mitm.savedMessages {
+	for _, buf := range mitm.aSavedMessages {
+		iv, buf := splitIV(buf, len(key))
+		msg := cpals.AESCBCDecrypt(key, iv, buf)
+		msgs = append(msgs, string(msg))
+	}
+	for _, buf := range mitm.bSavedMessages {
 		iv, buf := splitIV(buf, len(key))
 		msg := cpals.AESCBCDecrypt(key, iv, buf)
 		msgs = append(msgs, string(msg))
 	}
 	return msgs
+}
+
+type EvilMITMG struct {
+	Cryptor
+	server Server
+
+	snoopedMsgs []string
+}
+
+func NewEvilMITMG(g *big.Int) *EvilMITMG {
+	return &EvilMITMG{Cryptor: Cryptor{g: g, role: "MITMG"}}
+}
+
+func (mitm *EvilMITMG) Zero() *big.Int {
+	return big.NewInt(0)
+}
+
+func (mitm *EvilMITMG) One() *big.Int {
+	return big.NewInt(1)
+}
+
+func (mitm *EvilMITMG) P() *big.Int {
+	return cbig.BigCopy(nistP)
+}
+
+func (mitm *EvilMITMG) PMinus1() *big.Int {
+	return big.NewInt(0).Sub(nistP, big.NewInt(1))
+}
+
+func (mitm *EvilMITMG) Connect(server Server) {
+	mitm.server = server
+}
+
+func (mitm *EvilMITMG) Handshake(hello Request) Reply {
+	mitm.p = hello.p
+	mitm.A = hello.A
+
+	evilHandshake := Request{
+		p: hello.p,
+		g: mitm.g,
+		A: hello.A,
+	}
+
+	reply := mitm.server.Handshake(evilHandshake)
+	mitm.B = reply.B
+	//log.Printf("MITMG: A %s B %s", mitm.A, mitm.B)
+	return reply
+}
+
+func (mitm *EvilMITMG) Call(wireMsg []byte) []byte {
+	msg := mitm.aDecryptor()(wireMsg)
+	mitm.snoopedMsgs = append(mitm.snoopedMsgs, msg)
+	buf := mitm.bEncryptor()(msg)
+
+	wireReply := mitm.server.Call(buf)
+	reply := mitm.bDecryptor()(wireReply)
+	mitm.snoopedMsgs = append(mitm.snoopedMsgs, reply)
+	buf = mitm.aEncryptor()(reply)
+	return buf
+}
+
+func (mitm *EvilMITMG) decryptor(sessionKeyInt *big.Int) func(buf []byte) string {
+	return func(buf []byte) (smsg string) {
+		defer func() {
+			if r := recover(); r != nil {
+				smsg = fmt.Sprintf("Can't decrypt: %s", r)
+			}
+		}()
+
+		iv, buf := splitIV(buf, 16)
+		key := bigIntToKey(sessionKeyInt)
+		msg := cpals.AESCBCDecrypt(key, iv, buf)
+		return string(msg)
+	}
+}
+
+func (mitm *EvilMITMG) encryptor(sessionKeyInt *big.Int) func(msg string) []byte {
+	return func(msg string) []byte {
+		iv := cpals.RandomKey()
+		key := bigIntToKey(sessionKeyInt)
+		buf := cpals.AESCBCEncrypt(key, iv, []byte(msg))
+		buf = append(buf, iv...)
+		return buf
+	}
+}
+
+func (mitm *EvilMITMG) aSessionKey() *big.Int {
+	switch {
+	case cbig.BigEqual(mitm.g, mitm.One()):
+		// B is using g==1
+		// so b == B == 1
+		// so A's session key is B^a == 1^a == 1
+		// B's session key is A^b and we don't know b
+		return mitm.One()
+	case cbig.BigEqual(mitm.g, mitm.P()):
+		// g == p, so B == 0
+		return mitm.Zero()
+	case cbig.BigEqual(mitm.g, mitm.PMinus1()):
+		// g == -1, so all modexp == p == +1 or -1
+		return mitm.One()
+	default:
+		panic("unsupported g")
+	}
+}
+
+func (mitm *EvilMITMG) bSessionKey() *big.Int {
+	switch {
+	case cbig.BigEqual(mitm.g, mitm.One()):
+		// Dunno
+		return mitm.One()
+	case cbig.BigEqual(mitm.g, mitm.P()):
+		// Dunno
+		return mitm.One()
+	case cbig.BigEqual(mitm.g, mitm.PMinus1()):
+		// Dunno
+		return mitm.One()
+	default:
+		panic("unsupported g")
+	}
+}
+
+func (mitm *EvilMITMG) aDecryptor() func([]byte) string {
+	sessionKey := mitm.aSessionKey()
+	//log.Printf("MITM DEC aSessionKey %s", sessionKey)
+	return mitm.decryptor(sessionKey)
+}
+
+func (mitm *EvilMITMG) aEncryptor() func(string) []byte {
+	sessionKey := mitm.aSessionKey()
+	//log.Printf("MITM ENC aSessionKey %s", sessionKey)
+	return mitm.encryptor(sessionKey)
+}
+
+func (mitm *EvilMITMG) bDecryptor() func([]byte) string {
+	sessionKey := mitm.bSessionKey()
+	//log.Printf("MITM DEC bSessionKey %s", sessionKey)
+	return mitm.decryptor(sessionKey)
+}
+
+func (mitm *EvilMITMG) bEncryptor() func(string) []byte {
+	sessionKey := mitm.bSessionKey()
+	//log.Printf("MITM ENC bSessionKey %s", sessionKey)
+	return mitm.encryptor(sessionKey)
+}
+
+func (mitm *EvilMITMG) SnoopedMessages() []string {
+	return mitm.snoopedMsgs
 }
--- diffie/set5_test.go
+++ diffie/set5_test.go
@@ -6,22 +6,36 @@ import (
 	"github.com/jbert/cpals-go"
 )
 
-func TestS5C34(t *testing.T) {
-	t.Run("HONEST", testHonest)
-	t.Run("MITM", testMITM)
+func TestS5C35(t *testing.T) {
+	var m *EvilMITMG
+
+	//	t.Run("HONEST", testHonest)
+	mitm := NewEvilMITMG(m.One())
+	t.Run("MITM g=1", func(t *testing.T) { testMITM(t, mitm, true) })
+
+	mitm = NewEvilMITMG(m.P())
+	t.Run("MITM g=nistP", func(t *testing.T) { testMITM(t, mitm, true) })
+
+	mitm = NewEvilMITMG(m.PMinus1())
+	t.Run("MITM g=nistP-1", func(t *testing.T) { testMITM(t, mitm, true) })
 }
 
-func testMITM(t *testing.T) {
+func TestS5C34(t *testing.T) {
+	t.Run("HONEST", testHonest)
+	mitm := NewEvilMITM()
+	t.Run("MITM", func(t *testing.T) { testMITM(t, mitm, false) })
+}
+
+func testMITM(t *testing.T, mitm MITM, senderOnly bool) {
 	t.Log("Test MITM")
 	hs := NewHonestServer()
 	hc := NewHonestClient()
-	mitm := NewEvilMITM()
 
 	// This is what MITM can do ...
 	mitm.Connect(hs)
 	hc.Connect(mitm)
 
-	secretMessages := testComms(t, hc, hs)
+	secretMessages := testComms(t, hc, hs, senderOnly)
 
 	snoopedMsgs := mitm.SnoopedMessages()
 	for _, secretMsg := range secretMessages {
@@ -44,10 +58,10 @@ func testHonest(t *testing.T) {
 
 	hc := NewHonestClient()
 	hc.Connect(hs)
-	testComms(t, hc, hs)
+	testComms(t, hc, hs, false)
 }
 
-func testComms(t *testing.T, hc *HonestClient, hs *HonestServer) []string {
+func testComms(t *testing.T, hc *HonestClient, hs *HonestServer, senderOnly bool) []string {
 	secretMessages := []string{}
 
 	aMessage := "Yo, what's up?"
@@ -69,17 +83,21 @@ func testComms(t *testing.T, hc *HonestClient, hs *HonestServer) []string {
 
 	wireReply := hc.Send(wireMessage)
 	reply := hc.Decrypt(wireReply)
-	secretMessages = append(secretMessages, reply)
+	if !senderOnly {
+		secretMessages = append(secretMessages, reply)
+	}
 	if cpals.BytesEqual(wireReply, []byte(reply)) {
 		t.Fatalf("Decryption didn't do much")
 	} else {
 		t.Logf("Decryption changed bytes, must be good [%s] != [%s]", reply, string(wireReply))
 	}
 
-	if reply == expectedReply {
-		t.Logf("Server received [%s] and replied [%s] as hoped", aMessage, reply)
-	} else {
-		t.Fatalf("Failed to get correct reply: got %s expected %s", reply, expectedReply)
+	if !senderOnly {
+		if reply == expectedReply {
+			t.Logf("Server received [%s] and replied [%s] as hoped", aMessage, reply)
+		} else {
+			t.Fatalf("Failed to get correct reply: got %s expected %s", reply, expectedReply)
+		}
 	}
 
 	return secretMessages
